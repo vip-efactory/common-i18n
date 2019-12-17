3632f6cfeabb3a3050264fd392f09936dc822fc7
dbdu <1797890817@qq.com>
2019-12-17 10:42:37 +0800

    完善国际化信息的配置

---
+++ src/main/java/vip/efactory/common/i18n/config/I18NLocaleChangeInterceptor.java
@@ -0,0 +1,70 @@
+package vip.efactory.common.i18n.config;
+
+import org.springframework.util.ObjectUtils;
+import org.springframework.util.StringUtils;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.support.RequestContextUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+/**
+ * 默认的LocaleChangeInterceptor只能从请求中获取区域信息，这样是不够的
+ * 此处继承它，重写相关的方法，实现从header头中和请求中获取区域信息
+ */
+public class I18NLocaleChangeInterceptor extends LocaleChangeInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+            throws ServletException {
+        // 先从请求头中获取国际化的区域信息
+        String headerLocale = request.getHeader(getParamName());
+        String requestLocale = request.getParameter(getParamName());
+        // 若请求头中没有国际化参数，则从请求中获取，若都没有则为null
+        String parseLocale = StringUtils.isEmpty(headerLocale) ? (StringUtils.isEmpty(requestLocale) ? null : requestLocale) : headerLocale;
+        if (parseLocale != null) {
+            if (checkHttpMethod(request.getMethod())) {
+                LocaleResolver localeResolver = RequestContextUtils.getLocaleResolver(request);
+                if (localeResolver == null) {
+                    throw new IllegalStateException(
+                            "No LocaleResolver found: not in a DispatcherServlet request?");
+                }
+                try {
+                    localeResolver.setLocale(request, response, parseLocaleValue(parseLocale));
+                } catch (IllegalArgumentException ex) {
+                    if (isIgnoreInvalidLocale()) {
+                        if (logger.isDebugEnabled()) {
+                            logger.debug("Ignoring invalid locale value [" + parseLocale + "]: " + ex.getMessage());
+                        }
+                    } else {
+                        throw ex;
+                    }
+                }
+            }
+        }
+        // Proceed in any case.
+        return true;
+    }
+
+    /**
+     * 和父类中的方法一样，之所以在此处出现，是因为方法为私有，但是又需要用到
+     *
+     * @param currentMethod
+     * @return
+     */
+    private boolean checkHttpMethod(String currentMethod) {
+        String[] configuredMethods = getHttpMethods();
+        if (ObjectUtils.isEmpty(configuredMethods)) {
+            return true;
+        }
+        for (String configuredMethod : configuredMethods) {
+            if (configuredMethod.equalsIgnoreCase(currentMethod)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
--- src/main/java/vip/efactory/common/i18n/config/LocaleConfig.java
+++ src/main/java/vip/efactory/common/i18n/config/LocaleConfig.java
@@ -6,7 +6,11 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.validation.Validator;
 import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
-import org.springframework.web.servlet.i18n.AcceptHeaderLocaleResolver;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 import vip.efactory.common.i18n.service.ILocaleMsgSourceService;
 import vip.efactory.common.i18n.service.LocaleMsgSourceServiceImpl;
 
@@ -16,15 +20,16 @@ import java.util.Locale;
  * 国际化配置
  */
 @Configuration
-public class MessageSourceConfig {
+public class LocaleConfig implements WebMvcConfigurer {
 
     /*
      *  关于国际化处理器，Spring-webmvc5.2.2中支持：CookieLocaleResolver，SessionLocaleResolver，FixedLocaleResolver，AcceptHeaderLocaleResolver
     此处我使用AcceptHeaderLocaleResolver
      */
     @Bean
-    public AcceptHeaderLocaleResolver localeResolver() {
-        AcceptHeaderLocaleResolver ahlr = new AcceptHeaderLocaleResolver();
+    public LocaleResolver localeResolver() {
+//        AcceptHeaderLocaleResolver ahlr = new AcceptHeaderLocaleResolver();
+        SessionLocaleResolver ahlr = new SessionLocaleResolver();
         //设置默认区域, 中文简体
         ahlr.setDefaultLocale(Locale.SIMPLIFIED_CHINESE);
         return ahlr;
@@ -55,10 +60,30 @@ public class MessageSourceConfig {
 
     /**
      * 这是一个专门用来处理国际化的组件
-     *  以后可以优化使用配置的文件里的参数
+     * 以后可以优化使用配置的文件里的参数
      */
     @Bean
-    public ILocaleMsgSourceService iLocaleMsgSourceService(MessageSource messageSource){
+    public ILocaleMsgSourceService iLocaleMsgSourceService(MessageSource messageSource) {
         return new LocaleMsgSourceServiceImpl(messageSource);
+    }
+
+    /**
+     * 区域解析拦截器，拦截到相关的参数，自动变更区域设置。
+     */
+    @Bean
+    public LocaleChangeInterceptor localeChangeInterceptor() {
+        // 使用自己重写方法的解析器
+        LocaleChangeInterceptor lci = new I18NLocaleChangeInterceptor();
+        // 设置请求地址的参数,默认为：locale
+        // lci.setParamName(LocaleChangeInterceptor.DEFAULT_PARAM_NAME);
+        return lci;
+    }
+
+    /**
+     * 注册区域拦截器
+     */
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(localeChangeInterceptor());
     }
 }
