42242a2783a05abe9760974a6e84a52801c1948d
dbdu <1797890817@qq.com>
2020-01-20 14:44:20 +0800

    优化:key对应的value为空的判断逻辑

--- src/main/java/vip/efactory/common/i18n/enums/ErrorCodeUtil.java
+++ src/main/java/vip/efactory/common/i18n/enums/ErrorCodeUtil.java
@@ -5,6 +5,7 @@ import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 import vip.efactory.common.i18n.service.ILocaleMsgSourceService;
 import vip.efactory.common.i18n.util.FileUtil;
 
@@ -67,7 +68,7 @@ public class ErrorCodeUtil {
     public static String getMessage(String key, String... args) {
         String value = null;
         value = localeMessageSourceService.getMessage(key, args);
-        if (null == value || value.equals(key)) {
+        if (StringUtils.isEmpty(value) || value.equals(key)) {
             value = key;
             log.warn("missing key [{}]", key);
         }
